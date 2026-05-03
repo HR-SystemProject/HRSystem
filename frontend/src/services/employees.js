1c90668809acedba9b74c4379f8a18f0cc884915
rafahzyadat <rafahzyadat@gmail.com>
2026-05-03 13:50:16 +0300

    front1

--- frontend/src/services/employees.js
+++ frontend/src/services/employees.js
@@ -1,1 +1,40 @@
-// Employees API
+import axios from "axios";
+
+const API = "http://localhost:5000";
+
+const getToken = () => localStorage.getItem("token");
+
+export const createEmployee = async (data) => {
+  const res = await axios.post(`${API}/employees`, data, {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  });
+  return res.data;
+};
+
+export const updateEmployee = async (id, data) => {
+  const res = await axios.put(`${API}/employees/${id}`, data, {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  });
+  return res.data;
+};
+
+export const getAllEmployees = async () => {
+  const res = await axios.get(`${API}/employees`, {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  });
+  return res.data;
+};
+
+export const getEmployeeById = async (id) => {
+  const res = await axios.get(`${API}/employees/${id}`, {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  });
+  return res.data;
+};
+
+export const deleteEmployee = async (id) => {
+  const res = await axios.delete(`${API}/employees/${id}`, {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  });
+  return res.data;
+};
--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -1,3 +1,59 @@
-import { API } from "./api";
+import axios from "axios";
 
-export const getUsers = () => API.get("/users");
+const API = "http://localhost:5000";
+
+const getToken = () => localStorage.getItem("token");
+
+export const signup = async (data) => {
+  const res = await axios.post(`${API}/auth/register`, data);
+  return res.data;
+};
+
+export const login = async (data) => {
+  const res = await axios.post(`${API}/auth/login`, data);
+  return res.data;
+};
+
+export const logout = () => {
+  localStorage.removeItem("token");
+};
+
+export const updateProfile = async (data) => {
+  const res = await axios.put(`${API}/users/update-profile`, data, {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  });
+  return res.data;
+};
+
+export const forgetPassword = async (email) => {
+  const res = await axios.post(`${API}/auth/forget-password`, { email });
+  return res.data;
+};
+
+export const changePassword = async (data) => {
+  const res = await axios.post(`${API}/auth/change-password`, data, {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  });
+  return res.data;
+};
+
+export const getAllUsers = async () => {
+  const res = await axios.get(`${API}/users`, {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  });
+  return res.data;
+};
+
+export const getUserById = async (id) => {
+  const res = await axios.get(`${API}/users/${id}`, {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  });
+  return res.data;
+};
+
+export const deleteUser = async (id) => {
+  const res = await axios.delete(`${API}/users/${id}`, {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  });
+  return res.data;
+};
